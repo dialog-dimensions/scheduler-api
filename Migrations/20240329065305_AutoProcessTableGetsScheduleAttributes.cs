6c0badfd5fcf60f6f1c9478d2c9e5be86e32b6ba
dialog-dimensions <dialog.dimensions@gmail.com>
2024-03-30 04:50:22 +0300

    process tables migrations

---
+++ Migrations/20240329065305_AutoProcessTableGetsScheduleAttributes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Scheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoProcessTableGetsScheduleAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Strategy",
+                table: "Processes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+            
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduleStart",
+                table: "AutoScheduleProcesses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+            
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduleEnd",
+                table: "AutoScheduleProcesses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScheduleShiftDuration",
+                table: "AutoScheduleProcesses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScheduleEnd",
+                table: "AutoScheduleProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleShiftDuration",
+                table: "AutoScheduleProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleStart",
+                table: "AutoScheduleProcesses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Strategy",
+                table: "Processes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Migrations/20240329124341_AddStepsTable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Scheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStepsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Steps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProcessId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Steps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Steps_Processes_ProcessId",
+                        column: x => x.ProcessId,
+                        principalTable: "Processes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AutoScheduleProcesses_ScheduleStart",
+                table: "AutoScheduleProcesses",
+                column: "ScheduleStart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steps_ProcessId",
+                table: "Steps",
+                column: "ProcessId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Steps");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AutoScheduleProcesses_ScheduleStart",
+                table: "AutoScheduleProcesses");
+        }
+    }
+}
