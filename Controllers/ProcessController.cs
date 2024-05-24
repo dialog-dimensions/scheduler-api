74310d64f4bb8c92ebfe286d84072bd11e0c86f6
dialog-dimensions <dialog.dimensions@gmail.com>
2024-05-24 17:20:27 +0300

    Added an Endpoint to Run Scheduler Manually

--- Controllers/ProcessController.cs
+++ Controllers/ProcessController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SchedulerApi.DAL.Repositories.Interfaces;
 using SchedulerApi.Models.DTOs.OrganizationEntities;
-using SchedulerApi.Models.Organization;
+using SchedulerApi.Models.ScheduleEngine;
+using SchedulerApi.Services.ScheduleEngine.Interfaces;
 using SchedulerApi.Services.Workflows.Processes.Interfaces;
 
 namespace SchedulerApi.Controllers;
@@ -23,6 +25,18 @@ public class ProcessController : Controller
     {
         var process = _serviceProvider.GetRequiredService<IAutoScheduleProcess>();
         await process.Run(desk.ToEntity(), startDateTime, endDateTime, shiftDuration);
+        return Ok();
+    }
+
+    [HttpPost("runScheduler")]
+    [Authorize(Roles = "Admin,Manager")]
+    public async Task<ActionResult<ScheduleResults>> RunScheduler(string deskId, DateTime scheduleStartDateTime)
+    {
+        var scheduleEngine = _serviceProvider.GetRequiredService<IScheduler>();
+        var scheduleRepository = _serviceProvider.GetRequiredService<IScheduleRepository>();
+        var schedule = await scheduleRepository.ReadAsync((deskId, scheduleStartDateTime));
+        var results = await scheduleEngine.RunAsync(schedule!);
+        await scheduleRepository.AssignEmployees(deskId, scheduleStartDateTime, results.CompleteSchedule);
         return Ok();
     }
 }
