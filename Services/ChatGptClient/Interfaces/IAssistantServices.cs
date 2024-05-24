75c09215286eada45d431e3f2043d7a8e7027505
dialog-dimensions <dialog.dimensions@gmail.com>
2024-05-24 19:11:46 +0300

    GPT Version: New Services Interfaces

---
+++ Services/ChatGptClient/Interfaces/IAssistantServices.cs
@@ -0,0 +1,12 @@
+﻿using SchedulerApi.Models.ChatGPT;
+
+namespace SchedulerApi.Services.ChatGptClient.Interfaces;
+
+public interface IAssistantServices
+{
+    Task<string> CreateThreadAsync();
+    Task<bool> AddMessageToThreadAsync(string threadId, string message);
+    Task<bool> RunThreadAsync(string threadId, string assistantId);
+    Task<IEnumerable<Message>> ThreadListMessagesAsync(string threadId);
+    Task<Message> ReadLatestMessageAsync(string threadId);
+}
---
+++ Services/ChatGptClient/Interfaces/ISchedulerGptServices.cs
@@ -0,0 +1,10 @@
+﻿using SchedulerApi.Models.Entities;
+using SchedulerApi.Models.Entities.Workers;
+
+namespace SchedulerApi.Services.ChatGptClient.Interfaces;
+
+public interface ISchedulerGptServices
+{
+    Task<string> CreateSession((string, DateTime) scheduleKey, int employeeId, Dictionary<string, string>? otherInstructions = null);
+    Task ProcessIncomingMessage(string threadId, string incomingMessage, bool initialMessage = false);
+}
