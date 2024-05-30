225a86fa0788d24e1e8ce523074b86513abda85f
dialog-dimensions <dialog.dimensions@gmail.com>
2024-05-31 00:09:32 +0300

    Jobs Version: Removing traces of Scanners

--- Controllers/WorkflowController.cs
+++ Controllers/WorkflowController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SchedulerApi.Services.Workflows.Scanners;
+using SchedulerApi.Services.Workflows.Jobs;
 
 namespace SchedulerApi.Controllers;
 
@@ -8,120 +8,37 @@ namespace SchedulerApi.Controllers;
 [Route("api/[controller]")]
 public class WorkflowController : Controller
 {
-    private readonly IAutoScheduleScanner _scanner;
+    private readonly IJobSchedulingService _jobScheduling;
     private readonly IConfigurationSection _scannerParams;
     private readonly IConfigurationSection _processParams;
     
-    public WorkflowController(IAutoScheduleScanner scanner, IConfiguration configuration)
+    public WorkflowController(IJobSchedulingService jobScheduling, IConfiguration configuration)
     {
-        _scanner = scanner;
+        _jobScheduling = jobScheduling;
         _processParams = configuration.GetSection("Params:Processes:AutoSchedule");
         _scannerParams = configuration.GetSection("Params:Workflows:AutoScheduleScanner");
     }
 
 
-    [HttpPost("scanner/wake")]
+    [HttpPost("job/wake")]
     [Authorize(Roles = "Admin,Manager")]
-    public async Task<IActionResult> Wake()
+    public IActionResult Wake()
     {
-        await _scanner.Wake();
+        var jobId = IJobSchedulingService.ScannerJobId;
+        var cron = IJobSchedulingService.ScannerCron;
+        _jobScheduling.InitializeRecurringJob(jobId, cron);
         return Ok();
     }
 
-    [HttpPost("scanner/terminate")]
+    [HttpPost("job/terminate")]
     [Authorize(Roles = "Admin,Manager")]
     public IActionResult Terminate()
     {
-        _scanner.Terminate();
+        var jobId = IJobSchedulingService.ScannerJobId;
+        _jobScheduling.RemoveRecurringJob(jobId);
         return Ok();
     }
 
-    [HttpGet("scanner/status")]
-    [Authorize(Roles = "Admin,Manager")]
-    public ActionResult<bool> ScannerStatus()
-    {
-        return _scanner.ShouldRun;
-    }
-    
-    [HttpGet("process/status")]
-    [Authorize(Roles = "Admin,Manager")]
-    public ActionResult<string> ProcessStatus()
-    {
-        var processInProgress = _scanner.ProcessInProgress;
-        if (processInProgress is null)
-        {
-            return "No Process";
-        }
-    
-        return processInProgress.Status.ToString();
-    }
-    
-    [HttpGet("process/currentStep")]
-    [Authorize(Roles = "Admin,Manager")]
-    public ActionResult<string?> ProcessCurrentStep()
-    {
-        var processInProgress = _scanner.ProcessInProgress;
-        if (processInProgress is null)
-        {
-            return NoContent();
-        }
-    
-        return processInProgress.Status == TaskStatus.Running ? 
-            processInProgress.CurrentStep!.Task.Method.Name : 
-            processInProgress.Status.ToString();
-    }
-    
-    [HttpGet("process/strategy")]
-    [Authorize(Roles = "Admin,Manager")]
-    public ActionResult<IEnumerable<string>?> ProcessStrategy()
-    {
-        var processInProgress = _scanner.ProcessInProgress;
-        if (processInProgress is null)
-        {
-            return NoContent();
-        }
-    
-        return processInProgress.Strategy.StepsInStrategyToString().ToList();
-    }
-    
-    [HttpGet("process/startDateTime")]
-    [Authorize(Roles = "Admin,Manager")]
-    public ActionResult<DateTime?> ProcessStart()
-    {
-        var processInProgress = _scanner.ProcessInProgress;
-        if (processInProgress is null)
-        {
-            return NoContent();
-        }
-    
-        return processInProgress.ProcessStart;
-    }
-    
-    [HttpGet("process/fileWindowEnd")]
-    [Authorize]
-    public ActionResult<DateTime?> ProcessFileWindowEnd()
-    {
-        var processInProgress = _scanner.ProcessInProgress;
-        if (processInProgress is null)
-        {
-            return NoContent();
-        }
-    
-        return processInProgress.FileWindowEnd;
-    }
-    
-    [HttpGet("process/publishDateTime")]
-    [Authorize]
-    public ActionResult<DateTime?> ProcessPublishDateTime()
-    {
-        var processInProgress = _scanner.ProcessInProgress;
-        if (processInProgress is null)
-        {
-            return NoContent();
-        }
-    
-        return processInProgress.PublishDateTime;
-    }
 
     [HttpGet("process/params")]
     [Authorize(Roles = "Admin,Manager")]
@@ -131,7 +48,7 @@ public class WorkflowController : Controller
     }
     
     
-    [HttpGet("scanner/params")]
+    [HttpGet("job/params")]
     [Authorize(Roles = "Admin,Manager")]
     public ActionResult<Dictionary<string, string?>> GetScannerParams()
     {
