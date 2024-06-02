82d65c59d96dffd1f62f094d4109518105d6388f
dialog-dimensions <dialog.dimensions@gmail.com>
2024-06-02 18:11:30 +0300

    Added an empty authorization filter to the hangfire dashboard configuration.

---
+++ Services/JWT/AuthorizationFilters/JwtAuthorizationFilter.cs
@@ -0,0 +1,11 @@
+﻿using Hangfire.Dashboard;
+
+namespace SchedulerApi.Services.JWT.AuthorizationFilters;
+
+public class JwtAuthorizationFilter : IDashboardAuthorizationFilter
+{
+    public bool Authorize(DashboardContext context)
+    {
+        return true;
+    }
+}
