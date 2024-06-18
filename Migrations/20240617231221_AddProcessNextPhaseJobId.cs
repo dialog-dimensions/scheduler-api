e654996d101a3916fb840144e16f25526804abbf
dialog-dimensions <dialog.dimensions@gmail.com>
2024-06-18 04:40:17 +0300

    AddProcessNextPhaseJobId migration.

---
+++ Migrations/20240617231221_AddProcessNextPhaseJobId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Scheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessNextPhaseJobId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NextPhaseJobId",
+                table: "AutoScheduleProcesses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NextPhaseJobId",
+                table: "AutoScheduleProcesses");
+        }
+    }
+}
